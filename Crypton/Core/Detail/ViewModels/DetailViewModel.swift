2ad0890de03ef82ba00ffc88ed4eaf46fff950a3
Daniil Antsyferov <daniil.a@trustwallet.com>
2025-10-03 20:47:19 +0200

    Download coin details

---
+++ Crypton/Core/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,80 @@
+//
+//  DetailViewModel.swift
+//  Crypton
+//
+//  Created by Daniil Antsyferov on 29/9/25.
+//
+
+import Foundation
+import Combine
+
+class DetailViewModel: ObservableObject {
+
+    private var cancellables = Set<AnyCancellable>()
+    private let coinDetailService: CoinDetailDataService
+    @Published var coin: CoinModel
+
+    @Published var overviewStats: [StatisticModel] = []
+    @Published var additionalStats: [StatisticModel] = []
+
+    init(coin: CoinModel) {
+        self.coin = coin
+        self.coinDetailService = CoinDetailDataService(coin: coin)
+        addSubscribers()
+    }
+
+    private func addSubscribers() {
+        coinDetailService.$coinDetails
+            .combineLatest($coin)
+            .map(mapCoinDetails)
+            .sink { [weak self] returnedCoinDetails in
+                self?.overviewStats = returnedCoinDetails.overview
+                self?.additionalStats = returnedCoinDetails.additional
+            }
+            .store(in: &cancellables)
+    }
+
+    private func mapCoinDetails(details: CoinDetailModel?, coin: CoinModel) -> (overview: [StatisticModel], additional: [StatisticModel]) {
+
+        let price = coin.currentPrice.asCurrencyWith6Decimals()
+        let priceChange = coin.priceChangePercentage24H
+        let priceStat = StatisticModel(title: "Current Price", value: price, percentageChange: priceChange)
+
+        let marketCap = "$" + (coin.marketCap?.formattedWithAbbreviations() ?? "N/A")
+        let marketChange = coin.marketCapChangePercentage24H
+        let marketCapStat = StatisticModel(title: "Market Capitalization", value: marketCap, percentageChange: marketChange)
+
+        let rank  = "\(coin.rank)"
+        let rankStat = StatisticModel(title: "Rank", value: rank)
+
+        let volume = "$" + (coin.totalVolume?.formattedWithAbbreviations() ?? "N/A")
+        let volumeStat = StatisticModel(title: "Volume", value: volume)
+
+        let overview: [StatisticModel] = [priceStat, marketCapStat, rankStat, volumeStat]
+
+        let high = coin.high24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let highStat = StatisticModel(title: "24h High", value: high)
+
+        let low = coin.low24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let lowStat = StatisticModel(title: "24h Low", value: low)
+
+        let priceChange24h = coin.priceChange24H?.asCurrencyWith6Decimals() ?? "N/A"
+        let pricePercentChange24h = coin.priceChangePercentage24H
+        let priceChangeStat24h = StatisticModel(title: "24h Price Change", value: priceChange24h, percentageChange: pricePercentChange24h)
+
+        let marketCapChange = "$" + (coin.marketCapChange24H?.formattedWithAbbreviations() ?? "")
+        let marketCapPercentChange24h = coin.marketCapChangePercentage24H
+        let marketCapChangeStat = StatisticModel(title: "24h Market Cap Change", value: marketCapChange, percentageChange: marketCapPercentChange24h)
+
+        let blockTime = details?.blockTimeInMinutes ?? 0
+        let blockTimeString = blockTime == 0 ? "N/A" : "\(blockTime)"
+        let blockStat = StatisticModel(title: "Block Time", value: blockTimeString)
+
+        let hashing = details?.hashingAlgorithm ?? "N/A"
+        let hashingStat = StatisticModel(title: "Hashing algorithm", value: hashing)
+
+        let additional = [highStat, lowStat, priceChangeStat24h, marketCapChangeStat, blockStat, hashingStat]
+
+        return (overview, additional)
+    }
+}
--- Crypton/Core/Detail/Views/DetailView.swift
+++ Crypton/Core/Detail/Views/DetailView.swift
@@ -22,13 +22,88 @@ struct DetailLoadingView: View {
 
 struct DetailView: View {
 
-    let coin: CoinModel
+    @StateObject var vm: DetailViewModel
+
+    private let colums: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30
+
+    init(coin: CoinModel) {
+        _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
+    }
 
     var body: some View {
-        Text(coin.name)
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("")
+                    .frame(height: 150)
+
+                overviewTitle
+
+                Divider()
+
+                overviewGrid
+
+                additionalTitle
+
+                Divider()
+
+                additionalGrid
+
+            }
+        }
+        .navigationTitle(vm.coin.name)
+        .padding()
+
     }
 }
 
 #Preview {
-    DetailView(coin: dev.coin)
+    NavigationView {
+        DetailView(coin: dev.coin)
+    }
+}
+
+extension DetailView {
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    private var additionalTitle: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: colums,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.overviewStats) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+    }
+
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: colums,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: []) {
+                ForEach(vm.additionalStats) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+    }
 }
